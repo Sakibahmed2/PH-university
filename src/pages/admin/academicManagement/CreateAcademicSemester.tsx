82f0d2870843847f60a911cd996fe72aefd4a400
unknown <mauijaloskor@gmail.com>
2024-02-22 18:14:45 +0600

    Add Academic Semester Using RTK Query Mutation

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -6,6 +6,8 @@ import PHSelect from "../../../components/form/PHSelect";
 import { monthOptions } from "../../../constants/global";
 import { semesterOptions } from "../../../constants/semester";
 import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagment.api";
+import { toast } from "sonner";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
@@ -14,7 +16,9 @@ const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
 }));
 
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     const name = semesterOptions[Number(data.name) - 1].label;
 
     const semesterData = {
@@ -24,7 +28,14 @@ const CreateAcademicSemester = () => {
       startMonth: data.startMonth,
       endMonth: data.endMonth,
     };
-    console.log(semesterData);
+
+    try {
+      console.log(semesterData);
+      const res = await addAcademicSemester(semesterData);
+      console.log(res);
+    } catch (err) {
+      toast.error("Something went wrong");
+    }
   };
 
   return (
